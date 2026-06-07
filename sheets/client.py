00ebba0eed7ca6e54126e3d20509166cf9b8dfa5
dankusters <daniel.kusters@gmail.com>
2026-06-07 18:04:38 -0300

    atualiza referência para nova chave de serviço Google Sheets

--- sheets/client.py
+++ sheets/client.py
@@ -1,7 +1,7 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-CREDENTIALS_FILE = "sheets/bolaodacopa-496702-2ad378872f88.json"
+CREDENTIALS_FILE = "sheets/bolaodacopa-496702-3b6367a7a6b9.json"
 SPREADSHEET_NAME = "tabela_copa"
 FOLDER_ID = "1NOcia_b9GPEk7qR0wp5xMYB2baE7TNTtp0XEMlWLawo"
 
